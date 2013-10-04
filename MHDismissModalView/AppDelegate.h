42da79909c2ccfbe4f6d6b79cdae14708ef3f61f
Mario Hahn <mh@tailored-apps.com>
2013-10-04 11:15:29 +0200

    Initial Commit

---
+++ MHDismissModalView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MHDismissModalView
+//
+//  Created by Mario Hahn on 04.10.13.
+//  Copyright (c) 2013 Mario Hahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MHDismissModalView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MHDismissModalView
+//
+//  Created by Mario Hahn on 04.10.13.
+//  Copyright (c) 2013 Mario Hahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
